ad724cd300b021b9c6d82b1199a62512454115ae
tieTYT <tieTYT@gmail.com>
2014-08-28 18:11:11 -0700

    removing unused code

--- src/main/java/org/tieTYT/reverseSinglyLinkedList/ReverseAlgorithm.java
+++ src/main/java/org/tieTYT/reverseSinglyLinkedList/ReverseAlgorithm.java
@@ -22,28 +22,4 @@ public class ReverseAlgorithm {
         return frontFromLastIteration;
     }
 
-
-
-    private static Node[] reversedArray(Node head) {
-        int size = getListSize(head);
-
-        Node [] toBeReversed = new Node[size];
-
-        Node current = head;
-        for (int i = size - 1; i >= 0; i--) {
-            toBeReversed[i] = current;
-            current = current.getNext();
-        }
-        return toBeReversed;
-    }
-
-    private static int getListSize(Node head) {
-        int size = 0;
-        Node next = head;
-        while (next != null) {
-            next = next.getNext();
-            size++;
-        }
-        return size;
-    }
 }
